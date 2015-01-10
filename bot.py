f95bdda3e96c23e66efed89b48a7393bac6a3804
christoph halang <christoph.halang@gmail.com>
2015-01-10 06:11:45 -0800

    update bot

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-"""
-This program sends 10 random values between 0.0 and 1.0 to the /filter address,
-waiting for 1 seconds between each value.
-"""
 import argparse
 import random
 import time
@@ -53,44 +49,42 @@ def send(receiver, v):
   client.send(msg)
 
 def set_bulb_noisy():
-  v = 0.08
-  v += (random.random()*0.05)
-  flash = False
+  base = 0.2
+  factor = 0.1
+  v = base
+  v += (random.random()*factor)
   if random.random() > 0.99:
     v += 0.5
-    flash = True
-  send(15,v)
+    send(1,v)
+    time.sleep(0.15)
+    send(1,0)
+    send(2,v)
+    time.sleep(0.15)
+    send(2,0)
+    send(3,v)
+    time.sleep(0.15)
+    send(3,0)
+  else:
+    send(1,v)
+    v = base
+    v += (random.random()*factor)
+    time.sleep(0.01)
+    send(2,v)
+    v = base
+    v += (random.random()*factor)
+    time.sleep(0.01)
+    send(3,v)
 
-  time.sleep(0.1)
-  send(15,0)
 
-  v = 0.08
-  v += (random.random()*0.05)
-  if flash:
-    v += 0.5
-    time.sleep(0.1)
-  send(16,v)
-
-  v = 0.08
-  v += (random.random()*0.05)
-  if flash:
-    v += 0.5
-    time.sleep(0.1)
-
-  send(17,v)
 
 
 def set_neon():
-  msg = osc_message_builder.OscMessageBuilder(address = "/dmx/universe/5000/13")
+  msg = osc_message_builder.OscMessageBuilder(address = "/dmx/universe/5000/4")
   v = 0
   flash = False
   if random.random() > 0.99:
     flash = True
     v+=0.8
-  msg.add_arg(v)
-  msg = msg.build()
-  client.send(msg)
-  msg = osc_message_builder.OscMessageBuilder(address = "/dmx/universe/5000/14")
   msg.add_arg(v)
   msg = msg.build()
   client.send(msg)
@@ -109,15 +103,9 @@ if __name__ == "__main__":
   start_time = time.time()
 
   while True:
-    set_red_pulse()
-    time.sleep(0.01)
-    set_green_zero()
-    time.sleep(0.01)
-    set_blue_zero()
-    time.sleep(0.01)
     set_bulb_noisy()
-    time.sleep(0.01)
-    set_neon()
+    time.sleep(0.001)
+    #set_neon()
     time.sleep(0.001)
 
     pass
